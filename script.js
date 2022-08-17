88beef72273f3604d233fc4306e16fa171fd86b5
vitorlvr <vsouza954@gmail.com>
2022-08-17 20:21:01 -0300

    Criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 let canvas = document.getElementById("snake")
 let context = cavas.getContext("2d")
 let box = 32
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "lightscreen"
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
-criarBG()
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green"
+        context.fillRect(snake[i].y, box, box)
+    }
+}
+
+function iniciarJogo() {
+    criarBG()
+    criarCobrinha()
+
+    let snakeX =  snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeX -= box
+    if(direction == "up") snakeY -= box
+    if(direction == "down") snakeY += box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
